e3584ac748503b2ae75ffb47cfd932c3cecf2372
Neto Marçal <neto.marcal.dev@gmail.com>
2024-12-02 20:33:17 -0300

    Implementação inicial de projetos

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,11 +1,19 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Tarrefas from "@/views/Tarrefas.vue";
+import Projetos from "@/views/Projetos.vue";
 
-const rotas: RouteRecordRaw[] = [{
-    path: '/',
-    name: 'Tarefas',
-    component: Tarrefas
-}];
+const rotas: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'Tarefas',
+        component: Tarrefas
+    },
+    {
+        path: '/projetos',
+        name: 'Projetos',
+        component: Projetos
+    },
+];
 
 const roteador = createRouter({
     history: createWebHashHistory(),
